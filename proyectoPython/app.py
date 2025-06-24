df1e16a172931480f86f86f940511bce798b1d47
ErikLopez <espinozalopezerik@gmail.com>
2025-06-24 17:25:22 -0600

    inicio de interfaz

--- proyectoPython/app.py
+++ proyectoPython/app.py
@@ -5,3 +5,19 @@ from sparql_processor import run_sparql_query
 st.title("Film finder")
 film_name = st.text_input("Enter the name of a film")
 st.button("Search")
+
+if st.button("search") or film_name:
+    results = run_sparql_query(sparql_param=film_name,
+                                sparq_file="./sparql_queries/film_details.sparql")
+    if results["results"] ["bindings"]:
+        columns=["title", "runtime", "directorLabel","countryLabel"]
+
+        table_data=[]
+        for result in results["results"]["bindings"]:
+            row_data={}
+            for column in columns:
+                if result.get(column):
+                    row_data(column)=result[column][]
+
+    else:
+        st.write("No results found for the entered film name.")
