25bdb9bf1b83abd24970b34f08c50e8f8916a256
ErikLopez <espinozalopezerik@gmail.com>
2025-06-25 20:35:34 -0600

    ya funciona la interfaz y se corrijieron funciones del processor

--- proyectoPython/app.py
+++ proyectoPython/app.py
@@ -6,20 +6,25 @@ st.title("Film finder")
 film_name = st.text_input("Enter the name of a film")
 
 if st.button("Search") or film_name:
-    results = run_sparql_query(sparql_param=film_name,sparq_file="./proyectoPython/sparql_queries/film_details.sparql")
-    if results["results"] ["bindings"]:
-        columns=["title", "runtime", "directorLabel","countryLabel"]
+    results = run_sparql_query(sparql_param=film_name,
+                               sparql_file="./sparql_queries/film_details.sparql")
+    if results["results"]["bindings"]:
+        # definimos las columnas
+        columns = ["title", "runtime", "directorLabel", "countryLabel"]
 
-        table_data=[]
+        table_data = []
         for result in results["results"]["bindings"]:
-            row_data={}
+            row_data = {}
             for column in columns:
+                # al  iterar si existe la columna se da el valor
                 if result.get(column):
-                    row_data[column]=result[column]["value"]
-                else:
-                    row_data[column]="-"
+                    row_data[column] = result[column]["value"]
+                else:  # de lo contrario se pone el guion
+                    row_data[column] = "-"
             table_data.append(row_data)
-        table_data= pd.DataFrame(table_data)
-        st.dataFrame(table_data)
+        # se crea el df de pandas, en automatico se agregan los titulos
+        table_data = pd.DataFrame(table_data)
+        # se añade al panel
+        st.dataframe(table_data)
     else:
         st.write("No results found for the entered film name.")
--- proyectoPython/sparql_processor.py
+++ proyectoPython/sparql_processor.py
@@ -1,18 +1,22 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
+# creamos un cliente concectado al endpoint de DBpedia
+sparql_wrapper = SPARQLWrapper("http://dbpedia.org/sparql")
+sparql_wrapper.setReturnFormat(JSON)
 
-sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-
-sparql.setReturnFormat(JSON)
+# funcion que se le pasa el valor de consulta y la ruta al archivo .sparql
 
 
 def run_sparql_query(sparql_param, sparql_file):
+    # abre y lee el archivo sparql
     with open(sparql_file, "r") as file:
         query = file.read().replace("{sparql_param}", sparql_param)
 
     sparql_wrapper.setQuery(query)
 
     try:
-        ret = sparql.queryAndConvert()
+        # ejecuta la consulta y la convierte a JSON
+        ret = sparql_wrapper.queryAndConvert()
+        # se itera sobre los resultados y se retornan los datos obtenidos
         for r in ret["results"]["bindings"]:
             print(r)
         return ret
@@ -20,6 +24,7 @@ def run_sparql_query(sparql_param, sparql_file):
         print(e)
 
 
+# se ejecuta si el script es llamado directamente (no como modulo)
 if __name__ == "__main__":
     file_name = input("Enter the file name:")
     run_sparql_query(file_name, "./sparql_queries/film_details.sparql")
