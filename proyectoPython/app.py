bdf8df36b76309248ed36d9c8e5fe87a9fc705c6
ErikLopez <espinozalopezerik@gmail.com>
2025-06-27 22:35:10 -0600

    codigo con nodos

--- proyectoPython/app.py
+++ proyectoPython/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from sparql_processor import run_sparql_query
+from streamlit_agraph import Node, Edge, Config, agraph
 
 st.title("Film finder")
 film_name = st.text_input("Enter the name of a film")
@@ -34,5 +35,30 @@ if (st.button("Search") or film_name):
             # se agrega etiqueta que nos da el numero de peliculas distintas (nuevo query)
             film_number = results["results"]["bindings"][0]["filmNumber"]["value"]
             st.write("Number of film results: "+film_number)
+
+        graph = run_sparql_query(
+            sparql_param=film_name, sparql_file="./sparql_queries/film_construct.sparql")
+
+        nodes = {}
+        edges = []
+
+        for binding in graph["results"]["bindings"]:
+            s = binding["s"]["value"]
+            p = binding["p"]["value"]
+            o = binding["o"]["value"]
+
+            if s not in nodes:
+                subject_node = Node(id=s, label=s)
+                nodes[s] = subject_node
+            if o not in nodes:
+                object_node = Node(id=o, label=o)
+                nodes[o] = object_node
+
+            edge = Edge(source=s, target=o, label=p)
+            edges.append(edge)
+
+        config = Config(width=750, height=950, directed=True,
+                        physics=True, hierarchical=False)
+        agraph(nodes=list(nodes.values()), edges=edges, config=config)
     else:
         st.write("No results found for the entered film name.")
